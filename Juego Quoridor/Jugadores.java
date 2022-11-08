28cafc7d6ad373b098fd4e396410f942bff91aa1
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2022-11-07 23:01:11 -0500

    Sin terminar

--- Juego Quoridor/Jugadores.java
+++ Juego Quoridor/Jugadores.java
@@ -1,5 +1,13 @@
 public class Jugadores {
     public int x, y;
+    
+    public static void Jugador1() {
+
+    }//fin Jugador1
+    
+    public static void Jugador2() {
+        
+}//fin Jugador2
 
     //Constructor
     public Jugadores() {
--- Juego Quoridor/Tablero.java
+++ Juego Quoridor/Tablero.java
@@ -7,10 +7,10 @@ public class Tablero {
             System.out.print("|");
             for (int j = 0; j < 9; j++) {
                 if (i == jug1.getY() && j == jug1.getX()) {
-                    System.out.print("x|");
+                    System.out.print("o|");
                 }
                 else if (i == jug2.getY() && j == jug2.getX()) {
-                    System.out.print("o|");
+                    System.out.print("x|");
                 }
                 else {
                     System.out.print("_|");
@@ -19,4 +19,23 @@ public class Tablero {
             System.out.print("\n");
         }//fin for
     }//fin método DibujarTablero
+
+    public static void CrearTablero() {
+        System.out.println(" _ _ _ _ _ _ _ _ _");
+        for (int i = 0; i < 9; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 9; j++) {
+                if (i == jug1.getY() && j == jug1.getX()) {
+                    System.out.print("o|");
+                }
+                else if (i == jug2.getY() && j == jug2.getX()) {
+                    System.out.print("x|");
+                }
+                else {
+                    System.out.print("_|");
+                }//fin if
+            }//fin for
+            System.out.print("\n");
+        }//fin for
+    }//fin metodo CrearTablero
 }//fin class
