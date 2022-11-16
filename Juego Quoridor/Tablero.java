e80cf65462ec558c5fc534a2b2234f21eb3380ff
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2022-11-15 21:37:13 -0500

    Tablero terminado

--- Juego Quoridor/Tablero.java
+++ Juego Quoridor/Tablero.java
@@ -1,46 +1,58 @@
-
-
 public class Tablero {
-   public static void CrearTablero() {
-        System.out.println("  _ _ _ _ _ _ _ _ _");
-        for (int i = 0; i < 9; i++) {
-            System.out.print(i + "|");
-            for (int j = 0; j < 9; j++) {
-                if (i == 0 && j == 4) {
-                    System.out.print("o|");
-                }
-                else if (i == 8 && j == 4) {
-                    System.out.print("x|");
-                }
-                else {
-                    System.out.print("_|");
-                }//fin if
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void CrearTablero() {
+        System.out.println(" _____________________________________");
+        while (true) {
+            for (int i = 0; i < 9; i++) {
+                System.out.print(i + "|");
+                for (int j = 0; j < 9; j++) {
+                    if (i == 0 && j == 4) {
+                        System.out.print(" o |");
+                    }
+                    else if (i == 8 && j == 4) {
+                        System.out.print(ANSI_RED + " o " + ANSI_RESET + "|");
+                    }
+                    else {
+                        System.out.print("   |");
+                    }//fin if
+                } //fin for
+                System.out.print("\n +---+");
+                for (int k = 0; k < 8; k++) {
+                    System.out.print("---+");
+                } //fin for
+                System.out.print("\n");
             }//fin for
-            System.out.print("\n");
-        } //fin for
-        System.out.println("  0 1 2 3 4 5 6 7 8");
+            break;
+        }//fin while
+      System.out.println("   0   1   2   3   4   5   6   7   8");
     }//fin método DibujarTablero
 
-    /*public static void DibujarTablero() {
-        System.out.println(" _ _ _ _ _ _ _ _ _");
-        for (int i = 0; i < 9; i++) {
-            System.out.print("|");
-            for (int j = 0; j < 9; j++) {
-                if (i == jug1.getY() && j == jug1.getX()) {
-                    System.out.print("o|");
-                }
-                else if (i == jug2.getY() && j == jug2.getX()) {
-                    System.out.print("x|");
-                }
-                else {
-                    System.out.print("_|");
-                }//fin if
+    /*public static void CrearTablero() {
+        System.out.println(" _____________________________________");
+        while (true) {
+            for (int i = 0; i < 9; i++) {
+                System.out.print(i + "|");
+                for (int j = 0; j < 9; j++) {
+                    if (i == 0 && j == 4) {
+                        System.out.print(" o |");
+                    }
+                    else if (i == 8 && j == 4) {
+                        System.out.print(ANSI_RED + " o " + ANSI_RESET + "|");
+                    }
+                    else {
+                        System.out.print("   |");
+                    }//fin if
+                } //fin for
+                System.out.print("\n +---+");
+                for (int k = 0; k < 8; k++) {
+                    System.out.print("---+");
+                } //fin for
+                System.out.print("\n");
             }//fin for
-            System.out.print("\n");
-        }//fin for
-    }//fin metodo CrearTablero*/
-
-    public static void RegistrarMovimientos() {
-        
-    }//fin metodo RegistrarMovimientos
+            break;
+        }//fin while
+      System.out.println("   0   1   2   3   4   5   6   7   8");
+    }//fin método DibujarTablero */
 }//fin class
